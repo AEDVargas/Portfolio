adf8939daf533d2995d21280d4c86e3ebf97d8c9
Gnarles <edward.vargas1199@gmail.com>
2023-09-01 20:22:12 +0100

    Added simple nav bar, added stack boxes in main contents

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div class="container">
+            <h1 class="box">
+                <h2>Hello world!</h2>
+                <stack direction="row" spacing={2}>
+                    <item>Test 1</item>
+                    <item>Test 2</item>   
+                </stack>
+            </h1>
+        </div>
+    );
+}
+export default Home
